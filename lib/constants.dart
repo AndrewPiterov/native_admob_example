fbe5e8d57e8f85ed0b1f6b5278321c6e1a7d5850
Andrew Piterov <piterov1990@gmail.com>
2022-04-15 16:35:55 +0200

    [Add] missing ad ids for Android

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 class AppConstants {
-  static const nativeAdUnitId = 'ca-app-pub-3940256099942544/3986624511';
+  static final nativeAdUnitId = Platform.isAndroid
+      ? 'ca-app-pub-3940256099942544/2247696110'
+      : 'ca-app-pub-3940256099942544/3986624511';
   static const factoryId = 'listTile';
 }
